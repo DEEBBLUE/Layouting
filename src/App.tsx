c8cc64940bd9e0bde1fe867208632bf2cae30a26
Lifter <Sosite.my_dick@gmail.com>
2025-06-04 17:40:32 -0400

    Create style for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import Header from "./components/Header/Header";
+import "./App.scss"
+
+
 function App() {
   return (
     <>
+      <Header/>
+      <main className="main">
+      </main>
     </>
   )
 }
