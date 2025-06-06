45e6c9b33c6704d1f00d64db06947ff2b9bbe50d
Lifter <Sosite.my_dick@gmail.com>
2025-06-06 19:07:32 -0400

    Create slider to TeacherSection

---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,13 @@
+import type { FC } from "react";
+import "./slider.scss";
+
+
+const Slider: FC = () => {
+  return(
+    <div className="slider">
+       
+    </div>
+  )
+} 
+
+export default Slider;
