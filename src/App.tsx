9b0773d9ff528b6b3c369c58e0aef24be6ebdf92
Lifter <Sosite.my_dick@gmail.com>
2025-06-07 10:59:18 -0400

    Add TeacherSection

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import Header from "./components/Header/Header";
 import HeroSection from "./section/HeroSection/HeroSection";
 import AboutSection from "./section/AboutSection/AboutSection";
 import PerspectivesSection from "./section/PerspectivesSection/PerspectivesSection.tsx";
+import TeacherSection from "./section/TeacherSection/TeacherSection.tsx";
 
 import "./App.scss"
-
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
         <HeroSection/>
         <AboutSection/>
         <PerspectivesSection/>
+        <TeacherSection/>
       </main>
     </>
   )
